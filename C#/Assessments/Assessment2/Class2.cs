c9cb8ad47d2f0fdbf7e07bdfd39d81953e87384b
Poornimadevi Gunasekaran <poornimadevig@infinite.com>
2026-04-08 11:10:57 +0530

    Assesment2Completed

---
+++ C#/Assessments/Assessment2/Class2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    internal class Class2
+    {
+        static void CheckNumber(int num)
+        {
+            if (num < 0)
+                throw new Exception("Number cannot be Negative");
+        }
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter a Number: ");
+                int n = int.Parse(Console.ReadLine());
+                CheckNumber(n);
+                Console.WriteLine("Valid number");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ C#/Assessments/Assessment2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    abstract class Student
+    {
+        public String Name;
+        public int StudentId;
+        public double Grade;
+        public abstract bool IsPassed(double grade);
+
+    }
+    class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Student ug = new Undergraduate() { Name = "Mary", StudentId = 01, Grade = 75 };
+            Student g = new Graduate() { Name = "John", StudentId = 02, Grade = 78 };
+            Console.WriteLine("Undergraduate Passed: " + ug.IsPassed(ug.Grade));
+            Console.WriteLine("Undergraduate Passed: " + g.IsPassed(g.Grade));
+            Console.Read();
+        }
+    }
+}
