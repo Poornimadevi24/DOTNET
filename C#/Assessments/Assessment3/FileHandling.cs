5fa96ecb2dbfcc47205e81921a0e4324cfaa6ac9
Poornimadevi Gunasekaran <poornimadevig@infinite.com>
2026-04-16 11:08:01 +0530

    Assessment3 uploaded successfully

---
+++ C#/Assessments/Assessment3/FileHandling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge3
+{
+    internal class FileHandling
+    {
+        static void Main()
+        {
+            Console.Write("Enter file path: ");
+            string filepath = Console.ReadLine();
+
+            Console.Write("Enter text to append: ");
+            string text = Console.ReadLine();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filepath, true)) 
+                {
+                    writer.WriteLine(text);
+                }
+                Console.WriteLine("Text appended Successfully");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
